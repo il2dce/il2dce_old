680e680b6a8c55ffc5b0cee743f2c780c60b5f7b
il2dce <il2dce@googlemail.com>
2011-07-27 22:47:53 +0000

    Added prototype campaign. Changes SelectCampaignPage to load the prototype campaign on click of the new button.

--- IL2DCE/Pages/SelectCampaignPage.cs
+++ IL2DCE/Pages/SelectCampaignPage.cs
@@ -31,7 +31,44 @@ namespace IL2DCE
             public SelectCampaignPage()
                 : base("Select campaign", new SelectCampaign())
             {
+                FrameworkElement.bBack.Click += new System.Windows.RoutedEventHandler(bBack_Click);
+                FrameworkElement.bNew.Click += new System.Windows.RoutedEventHandler(bNew_Click);
+                FrameworkElement.bContinue.Click += new System.Windows.RoutedEventHandler(bContinue_Click);
 
+                // TODO: Make button visible when it is possible to continue a campaign.
+                FrameworkElement.bContinue.Visibility = System.Windows.Visibility.Hidden;
+            }
+
+            private SelectCampaign FrameworkElement
+            {
+                get
+                {
+                    return FE as SelectCampaign;
+                }
+            }
+
+            private IGame Engine;
+
+            public override void _enter(IGame play, object arg)
+            {
+                Engine = play;
+            }
+            
+            private void bBack_Click(object sender, System.Windows.RoutedEventArgs e)
+            {
+                Engine.gameInterface.PagePop(null);
+            }
+
+            private void bNew_Click(object sender, System.Windows.RoutedEventArgs e)
+            {
+                Engine.gameInterface.MissionLoad("$home/parts/IL2DCE/Campaigns/Prototype/Main.mis");
+                Engine.gameInterface.UIMainHide();
+                Engine.gameInterface.BattleStart();
+            }
+
+            private void bContinue_Click(object sender, System.Windows.RoutedEventArgs e)
+            {
+                Engine.gameInterface.PagePop(null);
             }
         }
     }
