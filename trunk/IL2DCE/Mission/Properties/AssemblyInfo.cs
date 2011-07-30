87aec37ff9c137c427598122ba5428678c5f1ee4
il2dce@googlemail.com <il2dce@googlemail.com@148f873a-ab3e-b280-30bf-669f859c2bf9>
2011-07-30 20:58:34 +0000

    Applied D(o not)R(epeat)Y(ourself) for AssemblyInfo.cs. GlobalAssemblyInfo.cs contains the global assembly information and is added to each project as link. To add the SVN revision number the GlobalAssemblyInfo.template contains a variable that is replaced by SubWcRev.

--- trunk/IL2DCE/Mission/Properties/AssemblyInfo.cs
+++ trunk/IL2DCE/Mission/Properties/AssemblyInfo.cs
@@ -2,32 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("Mission.Properties")]
+[assembly: AssemblyTitle("IL2DCE.Mission")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Mission.Properties")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
-[assembly: ComVisible(false)]
-
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("efd280d7-5acf-4bd6-a06d-3dbe5a4f98b5")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
