d08f2b51caa46430408743b4ead3ee0732929b47
il2dce <il2dce@googlemail.com>
2011-08-07 09:11:28 +0000

    Renamed project Engine to Core.

--- IL2DCE/Core/Properties/AssemblyInfo.cs
+++ IL2DCE/Core/Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("IL2DCE.Engine")]
+[assembly: AssemblyTitle("IL2DCE.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("df52f1e2-6b2a-4726-9f24-39b7c3adc6da")]
